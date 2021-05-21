e0550383fa100b7c7c8ffb7ea1d203b5712b748e
sadik4dcinema <sadikbrihmani@yahoo.com>
2021-05-21 19:07:28 +0200

    Create post-75352.js

---
+++ functions/post-75352.js
@@ -0,0 +1,20 @@
+exports.handler = async event => {
+  if (event.queryStringParameters.fbclid) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: 'https://offroad-lovers.com/1951-ford-coe-the-big-bandit/'
+      }
+    }
+  } else {
+    let pathName = event.path.split('/')[3].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: process.env.URL + '/' + pathName[0] + '/' + pathName[1] + '/'
+      }
+    }
+  }
+}
